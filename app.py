1e28bd32a29b2b1cb66bb00836295540269c9c26
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-10-05 11:11:41 +0200

    přidal jsem pole pro výběr pohlaví a tratě

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField
+from wtforms import StringField, SubmitField, DateField, SelectField
 from wtforms.validators import DataRequired, Length, Email, InputRequired
 
 app = Flask(__name__) # Argument __name__ je jméno modulu – Flask podle něj hledá soubory, které k aplikaci patří (static a templates).
@@ -10,9 +10,11 @@ app.secret_key = "secretKey"  # Secret key pro CSRF ochranu, měl by být uniká
 class RegisterForm(FlaskForm):
     first_name = StringField("Jméno", validators=[DataRequired(), Length(min=2, max=20)])
     second_name = StringField("Příjmení", validators=[DataRequired(), Length(min=2, max=20)])
+    sex = SelectField("Pohlaví", choices=[("man", "muž"), ("woman", "žena")])
     birthdate = DateField("Datum narození", validators=[DataRequired()])
     club_town = StringField("Oddíl/Město", validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField("Email", validators=[InputRequired(), Email()])
+    track = SelectField("Jakou trať", choices=[("long", "9 km"), ("short", "4 km"), ("kid", "Dětský závod")])
     submit = SubmitField("Odeslat")
 
 # Routing pro zobrazení a zpracování formuláře
@@ -24,11 +26,13 @@ def index():
     if form.validate_on_submit():  # Pokud byl formulář odeslán a data jsou validní
         first_name = form.first_name.data
         second_name = form.second_name.data
+        sex = form.sex.data
         birthdate = form.birthdate.data
         club_town = form.club_town.data
         email = form.email.data
+        track = form.track.data
 
-        return f"Vítej, {first_name}, {second_name}, {birthdate}, {club_town}, {email}!"
+        return f"Vítej, {first_name}, {second_name}, {sex}, {birthdate}, {club_town}, {email}, {track}!"
     
     return render_template("index.html", title = title_page, form=form)
 
